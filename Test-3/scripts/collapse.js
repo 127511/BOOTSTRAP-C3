fb486ca5572e94afa0916e5cbc00a19289a7f456
Jaimy Tieck <127511@student.drenthecollege.nl>
2020-09-21 10:22:40 +0200

    Made collapse examples under the form

---
+++ Test-3/scripts/collapse.js
@@ -0,0 +1,55 @@
+$("#hover1").hover(
+ function() {
+    $('#collapse1').collapse('show');
+  }, function() {
+    $('#collapse1').collapse('hide');
+  }
+);
+
+$("#hover2").hover(
+ function() {
+    $('#collapse2').collapse('show');
+  }, function() {
+    $('#collapse2').collapse('hide');
+  }
+);
+
+$("#hover3").hover(
+ function() {
+    $('#collapse3').collapse('show');
+  }, function() {
+    $('#collapse3').collapse('hide');
+  }
+);
+
+$("#hover4").hover(
+ function() {
+    $('#collapse4').collapse('show');
+  }, function() {
+    $('#collapse4').collapse('hide');
+  }
+);
+
+$("#hover5").hover(
+ function() {
+    $('#collapse5').collapse('show');
+  }, function() {
+    $('#collapse5').collapse('hide');
+  }
+);
+
+$("#hover6").hover(
+ function() {
+    $('#collapse6').collapse('show');
+  }, function() {
+    $('#collapse6').collapse('hide');
+  }
+);
+
+$("#hover7").hover(
+ function() {
+    $('#collapse7').collapse('show');
+  }, function() {
+    $('#collapse7').collapse('hide');
+  }
+);
