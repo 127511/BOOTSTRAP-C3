bd15d8794e22a58ce3f941b6ce6288f9ac691b16
Jaimy Tieck <127511@student.drenthecollege.nl>
2020-09-22 09:50:23 +0200

    additions to the form, bugfixes

--- Test-3/scripts/collapse.js
+++ Test-3/scripts/collapse.js
@@ -1,55 +1,146 @@
-$("#hover1").hover(
- function() {
+$('#hover1').mouseenter(function(){
+  if ($('#collapse1').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse1').one('hidden.bs.collapse', function () {
+      $('#collapse1').collapse('show');
+    });
+  } else {
     $('#collapse1').collapse('show');
-  }, function() {
+  }
+});
+$('#hover1').mouseleave(function(){
+  if ($('#collapse1').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse1').one('shown.bs.collapse', function () {
+      $('#collapse1').collapse('hide');
+    });
+  } else {
     $('#collapse1').collapse('hide');
   }
-);
+});
 
-$("#hover2").hover(
- function() {
+$('#hover2').mouseenter(function(){
+  if ($('#collapse2').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse2').one('hidden.bs.collapse', function () {
+      $('#collapse2').collapse('show');
+    });
+  } else {
     $('#collapse2').collapse('show');
-  }, function() {
+  }
+});
+$('#hover2').mouseleave(function(){
+  if ($('#collapse2').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse2').one('shown.bs.collapse', function () {
+      $('#collapse2').collapse('hide');
+    });
+  } else {
     $('#collapse2').collapse('hide');
   }
-);
+});
 
-$("#hover3").hover(
- function() {
+$('#hover3').mouseenter(function(){
+  if ($('#collapse3').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse3').one('hidden.bs.collapse', function () {
+      $('#collapse3').collapse('show');
+    });
+  } else {
     $('#collapse3').collapse('show');
-  }, function() {
+  }
+});
+$('#hover3').mouseleave(function(){
+  if ($('#collapse3').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse3').one('shown.bs.collapse', function () {
+      $('#collapse3').collapse('hide');
+    });
+  } else {
     $('#collapse3').collapse('hide');
   }
-);
+});
 
-$("#hover4").hover(
- function() {
+$('#hover4').mouseenter(function(){
+  if ($('#collapse4').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse4').one('hidden.bs.collapse', function () {
+      $('#collapse4').collapse('show');
+    });
+  } else {
     $('#collapse4').collapse('show');
-  }, function() {
+  }
+});
+$('#hover4').mouseleave(function(){
+  if ($('#collapse4').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse4').one('shown.bs.collapse', function () {
+      $('#collapse4').collapse('hide');
+    });
+  } else {
     $('#collapse4').collapse('hide');
   }
-);
+});
 
-$("#hover5").hover(
- function() {
+$('#hover5').mouseenter(function(){
+  if ($('#collapse5').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse5').one('hidden.bs.collapse', function () {
+      $('#collapse5').collapse('show');
+    });
+  } else {
     $('#collapse5').collapse('show');
-  }, function() {
+  }
+});
+$('#hover5').mouseleave(function(){
+  if ($('#collapse5').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse5').one('shown.bs.collapse', function () {
+      $('#collapse5').collapse('hide');
+    });
+  } else {
     $('#collapse5').collapse('hide');
   }
-);
+});
 
-$("#hover6").hover(
- function() {
+$('#hover6').mouseenter(function(){
+  if ($('#collapse6').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse6').one('hidden.bs.collapse', function () {
+      $('#collapse6').collapse('show');
+    });
+  } else {
     $('#collapse6').collapse('show');
-  }, function() {
+  }
+});
+$('#hover6').mouseleave(function(){
+  if ($('#collapse6').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse6').one('shown.bs.collapse', function () {
+      $('#collapse6').collapse('hide');
+    });
+  } else {
     $('#collapse6').collapse('hide');
   }
-);
+});
 
-$("#hover7").hover(
- function() {
+$('#hover7').mouseenter(function(){
+  if ($('#collapse7').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse7').one('hidden.bs.collapse', function () {
+      $('#collapse7').collapse('show');
+    });
+  } else {
     $('#collapse7').collapse('show');
-  }, function() {
+  }
+});
+$('#hover7').mouseleave(function(){
+  if ($('#collapse7').hasClass('collapsing')) {
+    console.log('true');
+    $('#collapse7').one('shown.bs.collapse', function () {
+      $('#collapse7').collapse('hide');
+    });
+  } else {
     $('#collapse7').collapse('hide');
   }
-);
+});
