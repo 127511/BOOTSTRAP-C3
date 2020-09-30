ef15646dee56cad3898322d90889518d4f79975c
Jaimy Tieck <127511@student.drenthecollege.nl>
2020-09-30 10:50:44 +0200

    Made back end of news page

---
+++ Test-3/scripts/newsfilter.js
@@ -0,0 +1,23 @@
+$("#cat_0").click(function(){
+  $(".catfilter_1").hide();
+  $(".catfilter_2").hide();
+  $(".catfilter_3").hide();
+});
+
+$("#cat_1").click(function(){
+  $(".catfilter_0").hide();
+  $(".catfilter_2").hide();
+  $(".catfilter_3").hide();
+});
+
+$("#cat_2").click(function(){
+  $(".catfilter_1").hide();
+  $(".catfilter_0").hide();
+  $(".catfilter_3").hide();
+});
+
+$("#cat_3").click(function(){
+  $(".catfilter_1").hide();
+  $(".catfilter_2").hide();
+  $(".catfilter_0").hide();
+});
